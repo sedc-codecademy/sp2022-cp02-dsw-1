86789c41fae40957a254ef7c5fbf007678d6ae03
ZoranK <zorankarbev@gmail.com>
2022-06-02 13:32:46 +0200

    Project's front-end skeleton setup, added basic routing, views and example component.

---
+++ client/src/views/categories.view.js
@@ -0,0 +1,10 @@
+export default class CategoriesView {
+    static render() {
+        return `
+            <div class="categories">
+                <h1>Categories</h1>
+                <p>This is the categories page of the website</p>
+            </div>   
+        `;
+    }
+}
