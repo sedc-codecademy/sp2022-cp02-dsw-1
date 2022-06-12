fc745708dbce322df06340c5455785a3c78c86db
anastasijadimkovska <anastasijad53@gmail.com>
2022-06-12 19:55:26 +0200

    updated classes

--- client/src/views/categories.view.js
+++ client/src/views/categories.view.js
@@ -3,9 +3,9 @@ export default class CategoriesView {
         return `
             <div class="categories">
             <div class="categoryContainer">
-            <div class="card">
+            <div class="card categoryCard">
                 
-                <img src="images/mainCategoryPics/pexels-wesley-carvalho-3622614 (1).jpg" class="card-img-top" alt="man dressed in yellow blouse">
+                <img src="images/mainCategoryPics/pexels-wesley-carvalho-3622614 (1).jpg" class="card-img-top categoryCardImg" alt="man dressed in yellow blouse">
                 <div class="card-body">
                     <div class="dropdown">
                         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
@@ -22,8 +22,8 @@ export default class CategoriesView {
                  
               </div>
               </div>
-              <div class="card">
-                <img src="images/mainCategoryPics/dom-hill-nimElTcTNyY-unsplash.jpg"  class="card-img-top" alt="woman dressed in yellow trousers">
+              <div class="card categoryCard">
+                <img src="images/mainCategoryPics/dom-hill-nimElTcTNyY-unsplash.jpg"  class="card-img-top categoryCardImg" alt="woman dressed in yellow trousers">
               
                 <div class="card-body">
                     <div class="dropdown">
