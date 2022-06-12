2c1d42ee315a67268785e0922cbfc72ce14f3cc0
anastasijadimkovska <anastasijad53@gmail.com>
2022-06-12 19:31:54 +0200

    main category page
    man-woman with sub-categories

--- client/src/views/categories.view.js
+++ client/src/views/categories.view.js
@@ -2,9 +2,45 @@ export default class CategoriesView {
     static render() {
         return `
             <div class="categories">
-                <h1>Categories</h1>
-                <p>This is the categories page of the website</p>
-            </div>   
+            <div class="categoryContainer">
+            <div class="card">
+                
+                <img src="images/mainCategoryPics/pexels-wesley-carvalho-3622614 (1).jpg" class="card-img-top" alt="man dressed in yellow blouse">
+                <div class="card-body">
+                    <div class="dropdown">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                          Man
+                        </a>
+                      
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Shirts</a></li>
+                            <li><a class="dropdown-item" href="#">Blouses</a></li>
+                            <li><a class="dropdown-item" href="#">Jeans</a></li>
+                            <li><a class="dropdown-item" href="#">Shorts</a></li>
+                        </ul>
+                      </div>
+                 
+              </div>
+              </div>
+              <div class="card">
+                <img src="images/mainCategoryPics/dom-hill-nimElTcTNyY-unsplash.jpg"  class="card-img-top" alt="woman dressed in yellow trousers">
+              
+                <div class="card-body">
+                    <div class="dropdown">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                          Woman
+                        </a>
+                      
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Shirts</a></li>
+                            <li><a class="dropdown-item" href="#">Blouses</a></li>
+                            <li><a class="dropdown-item" href="#">Jeans</a></li>
+                            <li><a class="dropdown-item" href="#">Shorts</a></li>
+                        </ul>
+                      </div>
+              </div>
+        </div>
+              
         `;
     }
 }
