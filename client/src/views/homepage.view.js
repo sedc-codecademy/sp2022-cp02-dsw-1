f89756fe47aac519791d01f1fdd9665f7cece61e
ZoranK <zorankarbev@gmail.com>
2022-06-14 17:16:18 +0200

    added homepage top section

--- client/src/views/homepage.view.js
+++ client/src/views/homepage.view.js
@@ -1,10 +1,36 @@
 export default class HomepageView {
     static render() {
         return `
-            <div class="homepage">
-                <h1>Welcome to the Drop Shipping Store Homepage</h1>
-                <p>This is the homepage of the website</p>
+        <!-- Homepage Top Section -->
+        <section class="homepage-top-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-md-6 homepage-top-section__link position-relative">
+                <a href="/#/women">
+                  <img src="../images/main-woman-medium.jpg" alt="female model">
+                </a>
+                <div class="homepage-top-section__button-container">
+                  <div class="homepage-top-section__button">
+                    <div class="homepage-top-section__button__slide"></div>
+                    <a href="#" class='homepage-top-section__link'>Women's Collection</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 homepage-top-section__link position-relative">
+                <a href="/#/men">
+                  <img src="../images/main-man-medium.jpg" alt="male model">
+                </a>
+                <div class="homepage-top-section__button-container">
+                  <div class="homepage-top-section__button">
+                    <div class="homepage-top-section__button__slide"></div>
+                    <a href="#" class='homepage-top-section__link'>Men's Collection</a>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+        </section>
+        <!-- End of Homepage Top Section -->
         `;
     }
 }
