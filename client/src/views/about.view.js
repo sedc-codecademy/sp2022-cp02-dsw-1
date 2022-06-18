2272429a0db3d5ba0f1062d1c73c3d9c7671353f
ZoranK <zorankarbev@gmail.com>
2022-06-18 20:24:18 +0200

    added privacy policy and about views

---
+++ client/src/views/about.view.js
@@ -0,0 +1,10 @@
+export default class AboutView {
+    static render() {
+        return `
+            <div class="error">
+                <h1>About</h1>
+                <p>This is About Page</p>
+            </div>
+        `;
+    }
+}
