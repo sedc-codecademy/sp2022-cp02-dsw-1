35d5c7d2cd7616b5562e697cc1319c3e928936c1
Dixie85 <98489775+Dixie85@users.noreply.github.com>
2022-06-21 00:53:03 +0200

    homepage_middle_v0.1

--- client/src/views/homepage.view.js
+++ client/src/views/homepage.view.js
@@ -1,5 +1,9 @@
+import RandomProductRendering from "../components/random.product_rendering.component";
+
 export default class HomepageView {
   static async render(resource, data) {
+
+    const randomProduct = await RandomProductRendering.render(resource, data);
 
     return `
         <!-- Homepage Top Section -->
@@ -32,6 +36,60 @@ export default class HomepageView {
           </div>
         </section>
         <!-- End of Homepage Top Section -->
+
+        <!-- Homepage Offers -->
+        <section style="width: 100% ;" class="py-5">
+        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div class="col">
+            <p class="text-center mt-5">Sneak peek to some of our SALE products</p>
+            <h2 class="text-center homepage__offers_H2">FUTURE TREND</h2>
+          </div>
+          </div>
+          <div class='container px-4 px-lg-5 mt-3'>
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            ${randomProduct[0]}
+            ${randomProduct[1]}
+            ${randomProduct[2]}
+            ${randomProduct[3]}
+            </div>
+          </div>
+        </section>
+        <!--End of Homepage Offers -->
+
+        <!-- Homepage About  -->
+        <section>
+          <div class="container homepage__about">
+              <div class="row justify-content-around">
+                  <div class="container col-12 col-lg-6 justify-content-center" >
+                    <h3 class="text-center my-4">WHAT WE ARE ALL ABOUT</h3>
+                    <p class="text-center px-4">
+                    Pellentesque vulputate porttitor malesuada. Nulla ut pharetra neque. 
+                    Phasellus quis dui est. Etiam mattis dui ullamcorper sapien sollicitudin accumsan. 
+                    Etiam vehicula nibh purus, vel lobortis tellus congue consequat. In hendrerit augue 
+                    ipsum, at scelerisque lorem tincidunt at. Vivamus tempus risus ac condimentum ullamcorper. 
+                    Ut id ipsum efficitur, dignissim metus ac, imperdiet enim. In maximus elementum porta. 
+                    Ut libero dolor, eleifend quis odio eget, luctus tempor nulla. Duis consectetur arcu sit 
+                    amet placerat auctor. Sed in tortor justo. Nunc feugiat ligula ut lacinia sollicitudin.
+                    Sed ut interdum nisl.
+                    Morbi commodo semper faucibus. Donec blandit nibh dolor, sed ornare metus hendrerit eu. 
+                    Sed vitae posuere ante. Mauris ac dictum purus. Curabitur ac ligula vel sapien gravida 
+                    facilisis sit amet a nibh. Morbi lobortis erat in malesuada iaculis. Nullam dictum lectus 
+                    ac facilisis convallis. Fusce varius ante tempus nisl scelerisque, sed ultrices ipsum 
+                    fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ornare augue. 
+                    Nulla quis ante sem. Morbi nec est risus. Aliquam porttitor imperdiet velit, sed mattis mauris 
+                    porttitor id. Maecenas laoreet felis mattis venenatis porta. Vestibulum sapien neque, imperdiet 
+                    at sem sit amet, efficitur ultricies magna.
+                    </p>                   
+                  </div>
+
+                  <div class="homepage__about__img__div col-12 col-lg-6 d-flex justify-content-center pt-5">
+                    <img class="rounded-3  shadow-lg mb-5 bg-body rounded" src=https://images.unsplash.com/photo-1531629685690-9137e85a4c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80 alt="WHAT WE ARE ALL ABOUT" >
+                  </div>
+              </div>
+          </div>
+        </section>
+        <!--End of Homepage About -->
+        
         `;
   }
 }
