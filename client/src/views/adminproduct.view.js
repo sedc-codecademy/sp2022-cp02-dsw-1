e5ac763bdc8ec4b7973e5a589a3c7df9aa1839a2
hacci <hacci_mk@hotmail.com>
2022-06-26 23:21:05 +0200

    admin product version 2

--- client/src/views/adminproduct.view.js
+++ client/src/views/adminproduct.view.js
@@ -2,85 +2,83 @@ import DashboardMenu from "../components/dashboardMenu";
 export default class adminproduct {
   static render() {
     return `
-   
-        <div class="content">
-        ${DashboardMenu.render()}
-        
-        <div id="mainButton">
+    ${DashboardMenu.render()}
+    <div id="mainButton">
 
         <a href="/#/admin" class="btn btn-dark">Recent orders</a>
         </div>
-      </div>
-   
-      <div class="container">
-     
-       <section class="panel panel-default">
-     <div class="panel-heading"> 
-     <h2 class="panel-title">Add new product</h2>
-     </div>
-     <div class="panel-body">
-       
-     <form action="designer-finish.html" class="form-horizontal" role="form">
-     
-       </div> <!-- form-group // -->
-       <div class="form-group">
-           <label for="Brand" class="col-sm-4 control-label">Product name</label>
-           <div class="col-sm-4">
-          <input type="text" class="form-control" name="Brand" id="name" placeholder="Product name">
+    <section class="h-100 gradient-form">
+    <div class="container py-5 h-100 shadow-lg p-3 mb-5 bg-body rounded">
+      <div class="row d-flex justify-content-center align-items-center h-100 ">
+        <div class="col-xl-10">
+          <div class="card rounded-3 text-black">
+            <div class="row g-0">
+              <div class="col-lg-6">
+                <div class="card-body p-md-5 mx-md-4">
+  
+                  <div class="text-center">
+                  <h4 class="mb-4">Please add product</h4>
+                  </div>
+                  <form class="form-admin-product">
+  
+                  <div class="form-floating mb-4">
+                  <input type="name" id="name" class="form-control" />
+                  <label class="form-label" for="name">Porduct name</label>
+                </div>
 
-           </div>
-               <div class="form-group">
-                   <label for="Brand" class="col-sm-4 control-label">Brand</label>
-                   <div class="col-sm-4">
-                  <input type="text" class="form-control" name="Brand" id="Brand" placeholder="Brand">
-                   </div>
-       </div> 
-       <!-- form-group // -->
-       <div class="form-group">
-           <label for="Category" class="col-sm-4 control-label">Category</label>
-           <div class="col-sm-4">
+                <div class="form-floating mb-4">
+                <input type="brand" id="brand" class="form-control"/>
+                <label class="form-label" for="brand">Brand</label>
+              </div>
+  
+              <div class="form-floating mb-4">
+                      <label class="form-label" for="Category"></label>
           <select class="form-control">
-           <option value="">Select</option>
-           <option value="Man">Man </option>
-           <option value="Women">Women </option>
+                  <option>Category</option>
+                  <option value="">Man </option>
+                  <option value="Women">Women </option>
           </select>
            </div>
-         </div> 
-         <!-- form-group // -->
-       <div class="form-group">
-           <label for="qty" class="col-sm-4 control-label">Quantity</label>
-           <div class="col-sm-4">
-          <input type="number" class="form-control" number="qty" id="qty" placeholder="Quantity.">
-           </div>
-           <div class="form-group">
-               <label for="price" class="col-sm-4 control-label">Price</label>
-               <div class="col-sm-4">
-              <input type="number" class="form-control" number="price" id="price" placeholder="$.">
-               </div>
-       </div> 
-       <!-- form-group // -->
-       <div class="form-group">
-         <label for="name" class="col-sm-4 control-label">Download</label>
-         <div class="col-sm-4">
-           <label class="control-label small" for="file_img">Picture (jpg/png):</label> <input type="file" name="file_img">
-         </div>
-       </div> 
-       <!-- form-group // -->
-       <hr>
-       <div class="form-group">
-         <div class="col-sm-offset-4 col-sm-9">
-           <button type="submit" class="btn btn-dark">Save</button>
-           <button type="submit" class="btn btn-dark">Cancel</button>
-         </div>
-       </div>
-     </form>
-     </div><!-- panel-body // -->
-     </section><!-- panel// -->
-     </div> <!-- container// -->
 
-        <script src="https://cdn.ckeditor.com/ckeditor5/44.1.0/classic/ckeditor.js"></script>
-        <script src"index.js"</script>
-        `;
+
+           <div class="form-floating mb-4">
+           <input type="number" id="qty" class="form-control" />
+             <label class="form-label" for="qty">Quantity</label>
+           </div>
+
+           <div class="form-floating mb-4">
+           <input type="number" id="qty" class="form-control"/>
+             <label class="form-label" for="price">Price $</label>
+           </div>
+           <div class="form-floating mb-4">
+           <label class="control-label small" for="file_img"><br>
+           Picture (jpg/png):</label> <input type="file" >
+                 <label class="form-label" for="name"></label>
+               </div>
+               <hr>
+                  <br>
+  
+                    <div class="d-flex align-items-center justify-content-center pb-4 buttons-save-cancel">
+                    <button type="button" class="btn btn-outline-light btn-dark btn-save">Save</button>
+                      <button type="button" class="btn btn-outline-light btn-dark btn-cancel">Cancel</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="col-lg-6 d-flex align-items-center card-logo-admin">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                <object data="images/logo/white-logo.svg" width="100em" height="200em"> </object> <br>
+                <h4 class="bb-4"Welcome to the product add page</h4>
+                  <p class="small mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>`;
   }
 }
-
