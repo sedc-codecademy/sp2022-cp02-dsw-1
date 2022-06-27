65bb1d5a1e98d73d35a5590b9e2c9dbacbf24c99
s-vedra <vedrastojanovska@gmail.com>
2022-06-28 00:00:08 +0200

    Update cart.view.js

--- client/src/views/cart.view.js
+++ client/src/views/cart.view.js
@@ -16,7 +16,7 @@ const addToCart = (item, forceUpdate = false) => {
   setCartItems(cartItems);
 };
 export default class CartView {
-  static async after_render() { }
+  static async after_render() {}
   static async render({ request: { id }, data }) {
     const products = await data;
     if (id) {
@@ -44,7 +44,6 @@ export default class CartView {
     });
 
     App.counterLoader();
-
     return /*html*/ `
         <div class="shopping-cart__card container">
         <div class="row">
@@ -53,13 +52,14 @@ export default class CartView {
                     <div class="row">
                         <div class="col"><h4><b>Shopping Cart</b></h4></div>
                         <div class="col align-self-center text-right text-muted">
-                            ${cartItems.length > 0
-        ? `${cartItems.reduce(
-          (a, c) => a + c.quantity,
-          0
-        )} items`
-        : "Empty"
-      } 
+                            ${
+                              cartItems.length > 0
+                                ? `${cartItems.reduce(
+                                    (a, c) => a + c.quantity,
+                                    0
+                                  )} items`
+                                : "Empty"
+                            } 
                         </div>
                     </div >
                 </div >
@@ -72,13 +72,12 @@ export default class CartView {
         <hr>
             <div class="row">
                 <div class="col" style="padding-left:0;">ITEMS ${cartItems.reduce(
-        (a, c) => a + c.quantity,
-        0
-      )}</div>
-                <div class="col text-right">$${filteredPrice.reduce(
-        (a, c) => a + c,
-        0
-      )}</div>
+                  (a, c) => a + c.quantity,
+                  0
+                )}</div>
+                <div class="col text-right">$${filteredPrice
+                  .reduce((a, c) => a + c, 0)
+                  .toFixed(2)}</div>
             </div>
             <form class="cart__summary__form">
                 <p>SHIPPING</p>
@@ -89,7 +88,7 @@ export default class CartView {
             <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
                 <div class="col">TOTAL PRICE</div>
                 <div class="col text-right">$ 
-                ${filteredPrice.reduce((a, c) => a + c, 0)}</div>
+                ${filteredPrice.reduce((a, c) => a + c, 0).toFixed(2)}</div>
             </div>
             <a href="/#/order" class="order-now-link link-light" ><button class="cart__summary__form__btn">ORDER NOW</button></a>
     </div>
