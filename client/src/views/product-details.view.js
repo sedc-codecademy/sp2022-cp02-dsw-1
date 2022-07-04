0d596e7c88a8556b2a2d3f07ae0ccad629303bec
ZoranK <zorankarbev@gmail.com>
2022-07-04 13:40:24 +0200

    added error message in product details if product is out of stock

--- client/src/views/product-details.view.js
+++ client/src/views/product-details.view.js
@@ -38,10 +38,13 @@ export default class ProductDetailsView {
       addToCartBtn.addEventListener("click", (e) => {
         e.preventDefault();
         const sizeErrorMessage = document.querySelector(".size__error-message");
+        // const quantityErrorMessage = document.querySelector(".quantity__error-message");
+
         if (sizeErrorMessage) {
           console.log(sizeErrorMessage);
           sizeErrorMessage.style.display = "none";
         }
+
         let productNumber = document.getElementById(`counter${id}`).innerHTML;
         memory.count = parseInt(productNumber);
         let cartItems = getCartItems();
@@ -87,6 +90,12 @@ export default class ProductDetailsView {
       const numberInStock = products[indexOfProduct].stock;
 
       plusButton[0].addEventListener("click", () => {
+        const quantityErrorMessage = document.querySelector(".quantity__error-message");
+        if (quantityErrorMessage) {
+          quantityErrorMessage.style.display = "none";
+        }
+
+        if (!selectSize) return
         let numberFromProduct = parseInt(
           document.getElementById(`counter${id}`).innerHTML
         );
@@ -95,7 +104,9 @@ export default class ProductDetailsView {
           document.getElementById(`counter${id}`).innerHTML =
             incremented.toString();
         } else {
-          alert("No more available in store :(");
+          console.log("quantityErrorMessage", quantityErrorMessage)
+          quantityErrorMessage.style.display = "block";
+          // alert("No more available in store :(");
           return;
         }
       });
@@ -104,15 +115,21 @@ export default class ProductDetailsView {
       const minusButton = document.getElementsByClassName(`fa-minus${id}`);
 
       minusButton[0].addEventListener("click", () => {
+        const quantityErrorMessage = document.querySelector(".quantity__error-message");
+        if (quantityErrorMessage) {
+          console.log(quantityErrorMessage);
+          quantityErrorMessage.style.display = "none";
+        }
         let numberFromProduct = parseInt(
           document.getElementById(`counter${id}`).innerHTML
         );
-        if (numberFromProduct > 1) {
+        if (numberFromProduct >= 1) {
           let incremented = (numberFromProduct -= 1);
           console.log("ulazi u ovu svakako");
           console.log(incremented, typeof incremented);
           document.getElementById(`counter${id}`).innerHTML =
             incremented.toString();
+          quantityErrorMessage.style.display = "none";
         } else {
           return;
         }
@@ -181,53 +198,49 @@ export default class ProductDetailsView {
               ${name}
               </h1>
               <div class="fs-1 mb-3">
-                ${
-                  !stock
-                    ? `<span class="text-muted text-decoration-line-through">
+                ${!stock
+        ? `<span class="text-muted text-decoration-line-through">
                 <small>$${price}</small>
               </span>`
-                    : discountPrice
-                    ? `$${discountPrice} 
+        : discountPrice
+          ? `$${discountPrice} 
                 <span class="text-muted text-decoration-line-through">
                   <small>$${price}</small>
                 </span>`
-                    : `$${price}`
-                }
+          : `$${price}`
+      }
               </div>
               <p class="lead singleProduct__description">
                 ${description}
               </p>
               <br/>
               <div class="d-flex"> 
-              ${
-                !stock
-                  ? `<div class="out-of-stock"><h2>Out of Stock</h2><a href="/#/${
-                      gender === "male" ? "men" : "women" || ""
-                    }" 
+              ${!stock
+        ? `<div class="out-of-stock"><h2>Out of Stock</h2><a href="/#/${gender === "male" ? "men" : "women" || ""
+        }" 
                         class="cart__back-to-shop-link nav-link">Back to shop</a></div>`
-                  : ` 
+        : ` 
                 <button class="page-link">
                   <i class="fas fa-minus fa-minus${id}"></i>
                 </button>
-                <div style="display:flex; align-items: center; justify-content: center;" class="page-link" id="counter${id}" > ${
-                      quantity ? quantity : 1
-                    }</div>
+                <div style="display:flex; align-items: center; justify-content: center;" class="page-link" id="counter${id}" > ${quantity ? quantity : 1
+        }</div>
                 <button class="page-link counter__plus">
                   <i" class="fas fa-plus fa-plus${id}"></i>
                 </button>
                 <select style="width: 5rem" class="form-select__singleProduct me-3 " required>
-                  <option value="" disabled selected>${
-                    choosenSize || "Size"
-                  }</option>
+                  <option value="" disabled selected>${choosenSize || "Size"
+        }</option>
                   ${size.map((s) => `<option value="${s}">${s}</option>`)}
                 </select>
                 <button class="btn btn-outline-dark flex-shrink-0 cart__btn-add-to-cart" type="button">
                   <i class="bi-cart-fill me-1"></i>
                   Add to cart
                 </button>`
-              }
+      }
                 </div>
                 <div class="size__error-message mt-3 mb-3 fs-5 px-3 py-2">Please choose size!</div>
+                <div class="quantity__error-message mt-3 mb-3 fs-5 px-3 py-2">No more items available in store<i class="bi bi-emoji-frown"></i></div>
             </div>
           </div>
         </div>
